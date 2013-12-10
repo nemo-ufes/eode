eff90a2aaaaee6d30e15d7b426bafad607706ded
danilloricardo <danillo.engcomp@gmail.com>
2013-12-10 19:16:47 -0200

    adding more menus.

--- br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/handlers/NewProjectHandler.java
+++ br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/handlers/NewProjectHandler.java
@@ -1,5 +1,12 @@
 package br.ufes.inf.nemo.odercp.rcpapp.handlers;
 
-public class NewProjectHandler {
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
+public class NewProjectHandler {
+	@Execute
+	public void execute(Shell shell) {
+		MessageDialog.openInformation(shell, "New", "New Project.");
+	}
 }
