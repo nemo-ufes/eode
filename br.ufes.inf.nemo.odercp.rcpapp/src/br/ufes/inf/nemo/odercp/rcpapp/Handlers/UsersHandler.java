81abeb4fdb20ce11ce74d477e1e8a932fd12a2cf
danilloricardo <danillo.engcomp@gmail.com>
2014-05-09 10:41:06 -0300

    new pages.

---
+++ br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/Handlers/UsersHandler.java
@@ -0,0 +1,22 @@
+package br.ufes.inf.nemo.odercp.rcpapp.Handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * TODO: document this type.
+ *
+ * @author Danillo Ricardo Celino (drcelino@inf.ufes.br)
+ * @version 1.0
+ */
+public class UsersHandler extends AbstractHandler {
+
+	/** @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent) */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
