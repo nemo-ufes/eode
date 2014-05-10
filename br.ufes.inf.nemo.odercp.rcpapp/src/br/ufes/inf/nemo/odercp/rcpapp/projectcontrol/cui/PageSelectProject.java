ae7ed6518dd523f79daee0ff476b3ac9ce753f84
danilloricardo <danillo.engcomp@gmail.com>
2014-05-10 00:56:22 -0300

    change page slect project.

--- br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/projectcontrol/cui/PageSelectProject.java
+++ br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/projectcontrol/cui/PageSelectProject.java
@@ -21,12 +21,12 @@ import br.ufes.inf.nemo.odercp.rcpapp.util.SwtUtil;
  */
 public class PageSelectProject {
 
-	protected Shell shlRegisterProject;
+	protected Shell shlSelectProject;
 	private String chosenProject;
 	String[] projects = ApplShowProjects.showProjects();
 	Button[] radios;
 	int i;// iterator to buttons actions.
-	Shell shlparentRegisterProject;
+	Shell shlparentSelectProject;
 
 	/**
 	 * Launch the application.
@@ -40,11 +40,11 @@ public class PageSelectProject {
 	public void open(Shell parent) {
 		Display display = Display.getDefault();
 		createContents(parent);
-		SwtUtil.centralizeparent(shlRegisterProject, parent);
+		SwtUtil.centralizeparent(shlSelectProject, parent);
 
-		shlRegisterProject.open();
-		shlRegisterProject.layout();
-		while (!shlRegisterProject.isDisposed()) {
+		shlSelectProject.open();
+		shlSelectProject.layout();
+		while (!shlSelectProject.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -57,23 +57,23 @@ public class PageSelectProject {
 	 * @wbp.parser.entryPoint
 	 */
 	protected void createContents(Shell parent) {
-		shlparentRegisterProject = parent;
-		shlRegisterProject = new Shell();
-		shlRegisterProject.setSize(515, 365);
-		shlRegisterProject.setText("Register Project");
+		shlparentSelectProject = parent;
+		shlSelectProject = new Shell();
+		shlSelectProject.setSize(515, 365);
+		shlSelectProject.setText("Register Project");
 
-		Button btnNewProject = new Button(shlRegisterProject, SWT.NONE);
+		Button btnNewProject = new Button(shlSelectProject, SWT.NONE);
 		btnNewProject.setImage(ResourceManager.getPluginImage("br.ufes.inf.nemo.odercp.rcpapp", "images/filenew.png"));
 		btnNewProject.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				PageCreateProject page = new PageCreateProject();
-				page.open(shlparentRegisterProject);
+				page.open(shlparentSelectProject);
 			}
 		});
 		btnNewProject.setBounds(10, 10, 28, 29);
 
-		Button btnOpenProject = new Button(shlRegisterProject, SWT.NONE);
+		Button btnOpenProject = new Button(shlSelectProject, SWT.NONE);
 		btnOpenProject.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -83,13 +83,13 @@ public class PageSelectProject {
 						chosenProject = radios[i].getText();
 					}
 				}
-				page.open(chosenProject, shlparentRegisterProject);
+				page.open(chosenProject, shlparentSelectProject);
 			}
 		});
 		btnOpenProject.setImage(ResourceManager.getPluginImage("br.ufes.inf.nemo.odercp.rcpapp", "images/fileopen.png"));
 		btnOpenProject.setBounds(44, 10, 28, 29);
 
-		Button btnDeleteProject = new Button(shlRegisterProject, SWT.NONE);
+		Button btnDeleteProject = new Button(shlSelectProject, SWT.NONE);
 		btnDeleteProject.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -98,7 +98,7 @@ public class PageSelectProject {
 						chosenProject = radios[i].getText();
 					}
 				}
-				if (MessageDialog.openConfirm(shlRegisterProject, "Confirmation", "Do you want Delete " + chosenProject + " ?")) {
+				if (MessageDialog.openConfirm(shlSelectProject, "Confirmation", "Do you want Delete " + chosenProject + " ?")) {
 					ApplDeleteProject.deleteProject(chosenProject);
 				}
 			}
@@ -107,7 +107,7 @@ public class PageSelectProject {
 		btnDeleteProject.setBounds(78, 10, 28, 29);
 		if (projects.length > 0) radios = new Button[projects.length];
 		for (i = 0; i < projects.length; i++) {
-			radios[i] = new Button(shlRegisterProject, SWT.RADIO);
+			radios[i] = new Button(shlSelectProject, SWT.RADIO);
 			radios[i].setText(projects[i]);
 			radios[i].setBounds(10, 45 + (30 * i), 750, 30);
 		}
