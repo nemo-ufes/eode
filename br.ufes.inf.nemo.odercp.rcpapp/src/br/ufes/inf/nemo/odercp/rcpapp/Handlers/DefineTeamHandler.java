153489845c8c63759bdebaffe766d4168d54da4c
danilloricardo <danillo.engcomp@gmail.com>
2014-05-19 17:02:26 -0300

    news Wizards.

--- br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/Handlers/DefineTeamHandler.java
+++ br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/Handlers/DefineTeamHandler.java
@@ -3,10 +3,14 @@ package br.ufes.inf.nemo.odercp.rcpapp.Handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import br.ufes.inf.nemo.odercp.rcpapp.projectcontrol.cui.wizards.WizardCreateProject;
 
 /**
  * TODO: document this type.
- *
+ * 
  * @author Danillo Ricardo Celino (drcelino@inf.ufes.br)
  * @version 1.0
  */
@@ -15,8 +19,6 @@ public class DefineTeamHandler extends AbstractHandler {
 	/** @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent) */
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
--- br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/projectcontrol/cui/PageCreateProject.java
+++ br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/projectcontrol/cui/PageCreateProject.java
@@ -1,88 +1,42 @@
 package br.ufes.inf.nemo.odercp.rcpapp.projectcontrol.cui;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Label;
 
-import br.ufes.inf.nemo.odercp.rcpapp.projectcontrol.cmt.ApplCreateProject;
-import br.ufes.inf.nemo.odercp.rcpapp.util.SwtUtil;
-
-public class PageCreateProject {
-
-	protected Shell shlCreateProject;
-	private Text txtNameproject;
-	private Text txtDescription;
+public class PageCreateProject extends WizardPage {
+	private Text textDescription;
 
 	/**
-	 * Launch the application.
+	 * Create the wizard.
+	 */
+	public PageCreateProject() {
+		super("Page Create Project");
+		setTitle("Page description Project ");
+		setDescription(" Description of Project");
+	}
+
+	/**
+	 * Create contents of the wizard.
 	 * 
-	 * @param args
+	 * @param parent
 	 */
-	public static void start() {
-		try {
-			PageCreateProject window = new PageCreateProject();
-			//window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
 
-	/**
-	 * Open the window.
-	 */
-	public void open(Shell parent) {
-		Display display = Display.getDefault();
-		createContents();
-		SwtUtil.centralizeparent(shlCreateProject, parent);
-		shlCreateProject.open();
-		shlCreateProject.layout();
-		while (!shlCreateProject.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
-	}
+		setControl(container);
 
-	/**
-	 * Create contents of the window.
-	 */
-	protected void createContents() {
-		shlCreateProject = new Shell();
-		shlCreateProject.setSize(442, 310);
-		shlCreateProject.setText("Create Project");
+		textDescription = new Text(container, SWT.BORDER | SWT.MULTI);
+		textDescription.setBounds(171, 10, 383, 217);
 
-		Label lblName = new Label(shlCreateProject, SWT.NONE);
-		lblName.setBounds(10, 29, 70, 17);
-		lblName.setText("Name:");
-
-		txtNameproject = new Text(shlCreateProject, SWT.BORDER);
-		txtNameproject.setBounds(112, 19, 295, 27);
-		txtNameproject.setText("");
-
-		Label lblDescription = new Label(shlCreateProject, SWT.NONE);
-		lblDescription.setBounds(10, 140, 89, 17);
+		Label lblDescription = new Label(container, SWT.NONE);
+		lblDescription.setBounds(10, 10, 95, 24);
 		lblDescription.setText("Description:");
+	}
 
-		txtDescription = new Text(shlCreateProject, SWT.BORDER | SWT.MULTI);
-		txtDescription.setBounds(112, 70, 295, 152);
-
-		Button btnCreate = new Button(shlCreateProject, SWT.NONE);
-		btnCreate.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				boolean ok = ApplCreateProject.CreateNewProject(
-						txtNameproject.getText(), txtDescription.getText());
-				if (ok)
-					shlCreateProject.close();
-			}
-		});
-		btnCreate.setBounds(180, 243, 91, 29);
-		btnCreate.setText("Create");
-
+	public String getTextDescription() {
+		return textDescription.getText();
 	}
 }
