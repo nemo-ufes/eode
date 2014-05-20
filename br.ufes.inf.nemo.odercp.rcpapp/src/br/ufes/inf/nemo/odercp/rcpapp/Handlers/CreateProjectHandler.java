f2fdd9aaffa4c49f59bf49f97e2ac785168bc017
danilloricardo <danillo.engcomp@gmail.com>
2014-05-20 16:35:31 -0300

    new wizard.

---
+++ br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/Handlers/CreateProjectHandler.java
@@ -0,0 +1,22 @@
+package br.ufes.inf.nemo.odercp.rcpapp.Handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import br.ufes.inf.nemo.odercp.rcpapp.projectcontrol.cui.wizards.WizardCreateProject;
+
+public class CreateProjectHandler extends AbstractHandler {
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		WizardCreateProject wizardCreateProject = new WizardCreateProject();
+		WizardDialog wizardDialog = new WizardDialog(HandlerUtil
+				.getActiveWorkbenchWindow(event).getShell(),
+				wizardCreateProject);
+		wizardDialog.open();
+		return null;
+
+	}
+}
