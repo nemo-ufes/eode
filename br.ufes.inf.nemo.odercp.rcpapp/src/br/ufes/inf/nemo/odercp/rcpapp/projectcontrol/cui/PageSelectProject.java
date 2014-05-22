8482d054a758a703de77852b5d5f07ccf9713881
danilloricardo <danillo.engcomp@gmail.com>
2014-05-22 15:15:14 -0300

    Wizard Select project.

--- br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/projectcontrol/cui/PageSelectProject.java
+++ br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/projectcontrol/cui/PageSelectProject.java
@@ -1,81 +1,45 @@
-/**
- * 
- */
 package br.ufes.inf.nemo.odercp.rcpapp.projectcontrol.cui;
 
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Button;
+
+import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 
-import br.ufes.inf.nemo.odercp.rcpapp.projectcontrol.cmt.ApplDeleteProject;
 import br.ufes.inf.nemo.odercp.rcpapp.projectcontrol.cmt.ApplShowProjects;
-import br.ufes.inf.nemo.odercp.rcpapp.util.SwtUtil;
-import org.eclipse.swt.widgets.Group;
 
-/**
- * @author Danillo Ricardo Celino (drcelino@inf.ufes.br)
- */
-public class PageSelectProject {
-
-	protected Shell shlSelectProject;
+public class PageSelectProject extends WizardPage {
+	protected String[] projects = ApplShowProjects.showProjects();
+	protected Button[] radios;
 	private String chosenProject;
-	String[] projects = ApplShowProjects.showProjects();
-	Button[] radios;
-	int i;// iterator to buttons actions.
-	Shell shlparentSelectProject;
 
-	/**
-	 * Launch the application.
-	 * 
-	 * @param args
-	 */
-
-	/**
-	 * Open the window.
-	 */
-	public void open(Shell parent) {
-		Display display = Display.getDefault();
-		createContents(parent);
-		SwtUtil.centralizeparent(shlSelectProject, parent);
-
-		shlSelectProject.open();
-		shlSelectProject.layout();
-		while (!shlSelectProject.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
+	public String getChosenProject() {
+		return chosenProject;
 	}
 
 	/**
-	 * Create contents of the window.
-	 * 
-	 * @wbp.parser.entryPoint
+	 * Create the wizard.
 	 */
-	protected void createContents(Shell parent) {
-		shlparentSelectProject = parent;
-		shlSelectProject = new Shell();
-		shlSelectProject.setSize(515, 365);
-		shlSelectProject.setText("Select Project");
+	public PageSelectProject() {
+		super("Page Select Project");
+		setTitle("Page Select Project ");
+		setDescription(" Select Project");
+	}
 
-				
-		Button btnSelect = new Button(shlSelectProject, SWT.NONE);
-		btnSelect.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
-		btnSelect.setBounds(209, 288, 91, 29);
-		btnSelect.setText("Select");
-		
-		if (projects.length > 0) radios = new Button[projects.length];
+	/**
+	 * Create contents of the wizard.
+	 * 
+	 * @param parent
+	 */
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+
+		setControl(container);
+		int i;
+		if (projects.length > 0)
+			radios = new Button[projects.length];
 		for (i = 0; i < projects.length; i++) {
-			radios[i] = new Button(shlSelectProject, SWT.RADIO);
+			radios[i] = new Button(this.getShell(), SWT.RADIO);
 			radios[i].setText(projects[i]);
 			radios[i].setBounds(10, 45 + (30 * i), 750, 30);
 		}
