eb0a694614a89f5b8a7bf8d27f731497103aa21b
danilloricardo <danillo.engcomp@gmail.com>
2014-07-17 11:20:29 -0300

    corigindo

--- br.ufes.inf.nemo.odercp.rcpapp/src/br/ufes/inf/nemo/odercp/rcpapp/knowledgeProcess/cpd/KSoftwareResource.java
+++
@@ -1,9 +0,0 @@
-package br.ufes.inf.nemo.odercp.rcpapp.knowledgeProcess.cpd;
-/**
- * @author Danillo Ricardo Celino (drcelino@inf.ufes.br)
- */
-public class KSoftwareResource extends KResource {
-
-	public KSoftwareResource() {}
-
-}
